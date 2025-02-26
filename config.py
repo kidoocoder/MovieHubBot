fa2356283c7a0802b686d52de6691460531e1066
kidoocoder <meinhuvicky@gmail.com>
2025-02-26 13:08:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ BOT_TOKEN = os.environ.get('7264247499:AAHZ1F3D5oPLOtLVomRtjrh1zMeOsf2kRbQ')  # 
 OWNER_ID = int(os.environ.get('OWNER_ID', '7799390858'))  # Replace with actual owner's Telegram ID
 SUPPORT_CHANNEL = "https://t.me/Guppppp_Shuppppp"  # Replace with actual support channel
 LOG_GROUP_ID = os.environ.get('-4684890007')  # Log group for movie requests
+DATABASE_URL = "postgres://kfcdtwea:jxgqtvc1ji7lSMjAhUp0QbxrE8Ut0t7N@fanny.db.elephantsql.com/kfcdtwea"
 
 # Welcome image URL - A default movie-themed image
 WELCOME_IMAGE_URL = "https://files.catbox.moe/v3q694.jpg"  # Default cinema/movie themed image
